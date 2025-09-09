6fd3a32546e35eee8aa3adac96e6ea451336d47d
rmasonh25 <rmasonh25@gmail.com>
2025-09-09 03:28:56 -0400

    Thyemleaf starter

---
+++ src/test/java/com/bujisoft/taming_thymeleaf/TamingThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bujisoft.taming_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamingThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
